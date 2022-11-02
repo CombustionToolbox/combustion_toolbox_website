d9a3fab1ca6086abffa87554fc261e22691a82e2
Alberto Cuadra Lara <acuadra@ing.uc3m.es>
2022-11-02 10:45:11 +0100

    Update: source

--- src/gui/functions/gui_SnapshotMenuSelected.m
+++
@@ -1,7 +0,0 @@
-function gui_SnapshotMenuSelected(UIFigure)
-    filter = {'*.pdf';'*.jpg';'*.png';'*.tif'};
-    [filename, filepath] = uiputfile(filter);
-    if ischar(filename)
-        exportapp(UIFigure, [filepath filename]);
-    end
-end
