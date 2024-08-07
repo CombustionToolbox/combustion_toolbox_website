a1ece1049bb86d95689df78cce128d2e2d9ef8dd
AlbertoCuadra <acuadra@ing.uc3m.es>
2024-08-07 10:11:13 +0200

    Update: test 5

--- docs/make.bat
+++ docs/make.bat
@@ -26,11 +26,11 @@ if errorlevel 9009 (
 	exit /b 1
 )
 
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O% --keep-going
 goto end
 
 :help
-%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O% --keep-going
 
 :end
 popd
