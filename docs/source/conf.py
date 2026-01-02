c4fe83f2a0d4657f901752ad23f7a32ff6b1ae39
AlbertoCuadra <acuadra@ing.uc3m.es>
2026-01-02 09:48:10 +0100

    Update: banner

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -221,7 +221,7 @@ html_css_files = [
 # }
 
 html_theme_options = {
-    "announcement": '<a class="no-underline"; href="https://www.researchgate.net/publication/398856092_Combustion_Toolbox_An_open-source_thermochemical_code_for_gas-_and_condensed-phase_problems_involving_chemical_equilibrium"; target="_blank"> The article of the Combustion Toolbox is now available on ResearchGate!</a>',
+    "announcement": '<a class="no-underline"; href="https://www.researchgate.net/publication/398856092_Combustion_Toolbox_An_open-source_thermochemical_code_for_gas-_and_condensed-phase_problems_involving_chemical_equilibrium"; target="_blank"> The published article of the Combustion Toolbox is now available on ResearchGate!</a>',
     "sidebar_hide_name": True,
     'light_logo': 'img/logo_CT_version.svg',
     'dark_logo': 'img/logo_CT_version_dark.svg',
